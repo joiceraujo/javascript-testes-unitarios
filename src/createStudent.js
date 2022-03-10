89aa9f6104ba1337ccf6a2721fec22f0f2470473
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-10 14:42:07 -0300

    Adiciona funçao createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const objStudent = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objStudent;
+};
 module.exports = createStudent;
