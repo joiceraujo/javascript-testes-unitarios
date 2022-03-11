fa5790804e7f7c3f7e3cdc59deefa6de044e72ce
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-11 09:17:10 -0300

    Finalizado a funçao myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,12 +20,11 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    // eslint-disable-next-line no-shadow
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
