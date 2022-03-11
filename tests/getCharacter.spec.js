4175bff7be90a2a6e9b78afd009c8b98b830d661
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-11 09:23:31 -0300

    Adicionado testes getCharacter

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -45,10 +45,16 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter()).toBeUndefined();
+    // Teste se a função retorna o objeto correto para o parâmetro 'Arya','Arya'
+    expect(typeof getCharacter('Arya')).toBe('object');
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(typeof getCharacter('Brienne')).toBe('object');
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(typeof getCharacter('Melissandre')).toBe('object');
     // Teste se os parâmetros não são Case Sensitive.
+    expect(getCharacter('ARYA')).not.toBeUndefined;
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('bolinha')).toBeUndefined
   });
 });
